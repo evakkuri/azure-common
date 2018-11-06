7246609b0ccd504afa60e0ef98ba7e3dd20b93e9
Elias Vakkuri <elias.vakkuri@gmail.com>
2018-11-06 18:11:33 +0200

    Add first version of blob container template and test scripts

---
+++ pstestscripts/deploy-storageaccountwithcontainer.ps1
@@ -0,0 +1,37 @@
+# Start timer
+$ScriptStart = (Get-Date)
+
+# Define common variables
+$rgName = "commonservices"
+$location = "northeurope"
+$deploymentName = "deploymentTest" + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')
+
+$workFolder = $PSScriptRoot
+$parentFolder = Split-Path -Path $PSScriptRoot -Parent
+
+$deploymentTemplateFile = $workFolder + "\test-storageaccountwithcontainer.json"
+
+# Create resource group
+New-AzureRmResourceGroup -Name $rgName -Location $location -Force
+
+# Define deployment parameters
+$deploymentParams = @{
+    "Name" = $deploymentName
+    "ResourceGroupName" = $rgName
+    "TemplateFile" = $deploymentTemplateFile
+    "Mode" = "Complete"
+    "Verbose" = $true
+}
+
+# Run deployment script
+New-AzureRmResourceGroupDeployment @deploymentParams -Force
+
+# Do any additional actions
+#$storageAccountKey = (Get-AzureRmResourceGroupDeployment -ResourceGroupName $rgName -Name $deploymentName).Outputs.storageAccountKey.value
+
+# Remove resource group
+#Remove-AzureRmResourceGroup -Name $rgName -Force -Verbose
+
+# Stop timer
+$ScriptEnd = (Get-Date)
+Write-Host "This script took $($ScriptEnd - $ScriptStart) seconds to run"
